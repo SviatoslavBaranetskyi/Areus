c65682e493eb5226a6c2f8b0094ccb0ac6c71b20
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-01-26 18:41:33 +0200

    Added login page view

--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import View
 
 
 def index(request):
-    return HttpResponse('Hello World!')
+    if request.method == "POST":
+        return HttpResponseRedirect('/login')
+    return render(request, 'app/index.html')
+
+
+def login_page_view(request):
+    entered_data = request.POST
+    return render(request, "app/main-page.html", {
+        "Host": entered_data['host'],
+        "Username": entered_data['username'],
+        "Password": 'Secret'
+    })
+
+
+'''class LoginPageView(View):
+    def get(self, request):
+        return render(request, 'app/index.html')
+
+    def post(self, request):
+        return HttpResponse("Hello World!")'''
