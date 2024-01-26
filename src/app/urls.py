1b3093d858288aad1fbb6c310f191a977c759571
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-01-26 18:40:58 +0200

    Added login path

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="starting-page"),
+    path("", views.index, name="index-page"),
+    path("login", views.login_page_view, name="login-page"),
 ]
