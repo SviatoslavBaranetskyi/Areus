be343956819d5d4d1ae644ac3a6caa71cf61af06
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-01-30 23:34:43 +0200

    Added session engine

--- src/areus/settings.py
+++ src/areus/settings.py
@@ -127,3 +127,5 @@ STATIC_ROOT = "/var/www/static"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
