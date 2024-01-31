74c087ccdca95dad6e63dfa6036b06e86faff8e7
danylo829 <evg829@gmail.com>
2024-02-01 01:12:38 +0200

    added get_databases endpoint

--- src/app/urls.py
+++ src/app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.MainPageView.as_view(), name='main-page'),
     path('login', views.login, name='login-page'),
+    path('get_databases', views.get_databases, name='get_databases')
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
@@ -29,6 +29,28 @@ def login(request):
     form = LoginForm()
     return render(request, 'app/login.html', {'form': form})
 
+def get_databases(request):
+    if not 'host' in request.session:
+        return HttpResponse(status=403)
+
+    db = connect(host=request.session['host'], username=request.session['username'], password=request.session['password'])
+    cursor = db.cursor()
+
+    # Execute query to get list of databases
+    cursor.execute("SHOW DATABASES")
+
+    # Fetch all databases
+    databases = cursor.fetchall()
+    print(databases)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
+
+    # Convert the list of databases to a dictionary
+    data = {"databases": [db[0] for db in databases]}
+
+    return JsonResponse(data)
 
 class MainPageView(View):
     @staticmethod
