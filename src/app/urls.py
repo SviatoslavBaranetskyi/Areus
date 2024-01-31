098bfa4de4016d100be973b00b62cb112b1ae4de
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-01-31 22:14:48 +0200

    Changed urls

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='index-page'),
-    path('main', views.MainPageView.as_view(), name='main-page'),
+    path('', views.MainPageView.as_view(), name='main-page'),
+    path('login', views.login, name='login-page'),
 ]
