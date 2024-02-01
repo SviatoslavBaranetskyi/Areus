8fb3900bbf065b487fa3c969ef8fe9fa7389c3d9
danylo829 <evg829@gmail.com>
2024-02-02 00:20:48 +0200

    implemented refresh button

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
-    console.log("loaded")
-    $.ajax({
-      url: 'get_databases',
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-        console.log(JSON.stringify(data))
-      },
-      error: function(xhr, status, error) {
-        console.error('Error: ', status, error);
-      }
-    });
+  fetchDatabases();
+
+  $(".tile").on("click", "#refresh-databases", function(){
+    fetchDatabases();
   });
+});
+
+function fetchDatabases() {
+  $.ajax({
+    url: 'get_databases',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      displayDatabases(data.databases, data.host);
+    },
+    error: function(xhr, status, error) {
+      console.error('Error: ', status, error);
+    }
+  });
+}
+
+function displayDatabases(databases, host) {
+  var databaseList = $('#database-list');
+  var content = '<h2>' + host + '</h2>' + '<h3>Databases<img id="refresh-databases" src="static/icons/refresh.png" alt="refresh"></h3>'
+  var listHTML = '<ul>';
+  
+  databases.forEach(function(database) {
+      listHTML += '<li><a href="/">' + database + '</a></li>';
+  });
+
+  listHTML += '</ul>';
+  content += listHTML;
+  databaseList.html(content);
+  databaseList.hide().slideDown(500);
+}
