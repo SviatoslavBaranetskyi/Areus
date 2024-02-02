5b3be78301c14c56ae097cba7ace70574144ff45
danylo829 <evg829@gmail.com>
2024-02-03 01:13:36 +0200

    display tables

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -4,6 +4,20 @@ $(document).ready(function() {
   $(".tile").on("click", "#refresh-databases", function(){
     fetchDatabases();
   });
+  $(".tile").on("click", "#database", function(){
+    var databaseName = $(this).text();
+    $.ajax({
+      url: 'api/get-tables/',
+      method: 'GET',
+      data: { database: databaseName },
+      success: function(data) {
+        generateTablesTable(data);
+      },
+      error: function(error) {
+        console.error('Error fetching tables:', error);
+      }
+    });
+  });
 });
 
 function fetchDatabases() {
@@ -22,15 +36,46 @@ function fetchDatabases() {
 
 function displayDatabases(databases, host) {
   var databaseList = $('#database-list');
-  var content = '<h2>' + host + '</h2>' + '<h3>Databases<img id="refresh-databases" src="static/icons/refresh.png" alt="refresh"></h3>'
+  var databasesElement = '<h3>Databases<img id="refresh-databases" src="static/icons/refresh.png" alt="refresh"></h3>';
+  var content = '<h2>' + host + '</h2>' + databasesElement;
   var listHTML = '<ul>';
   
   databases.forEach(function(database) {
-      listHTML += '<li><a href="/">' + database + '</a></li>';
+      listHTML += '<li><a id="database">' + database + '</a></li>';
   });
 
   listHTML += '</ul>';
   content += listHTML;
+  databaseList.hide().fadeOut();
   databaseList.html(content);
-  databaseList.hide().slideDown(500);
+  databaseList.hide().fadeIn();
+}
+
+function generateTablesTable(data) {
+  var tableHTML = '<table>' +
+    '<thead>' +
+      '<tr class="head">' +
+        '<th>table</th>' +
+        '<th>rows</th>' +
+        '<th>size</th>' +
+        '<th>collation</th>' +
+      '</tr>' +
+    '</thead>' +
+    '<tbody>';
+
+  $.each(data.tables, function(tableName, tableData) {
+    tableHTML += '<tr>' +
+      '<td>' + tableName + '</td>' +
+      '<td>' + tableData.rows + '</td>' +
+      '<td>' + tableData.size + '</td>' +
+      '<td>' + tableData.collation + '</td>' +
+    '</tr>';
+  });
+
+  tableHTML += '</tbody></table>';
+
+  $('#table-container').hide().fadeOut();
+  $('#database-name').html(data.database);
+  $('#table-container').html(tableHTML);
+  $('#table-container').hide().fadeIn();
 }
