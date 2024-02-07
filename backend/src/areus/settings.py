0f84f42011e4eb500607747ff1779c79b42f2c75
danylo829 <evg829@gmail.com>
2024-02-08 00:19:53 +0200

    implemented frontend dockerization

--- backend/src/areus/settings.py
+++ backend/src/areus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y^$d44gdibq84q6^65$mxkdtsit%_f7fxd+yy8uasz_-xafpvc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend']
 
 
 # Application definition
