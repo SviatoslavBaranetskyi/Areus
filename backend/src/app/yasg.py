d8c9e4a16353aadaffc7ced92f83c908a5307798
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-02-13 17:14:58 +0200

    changed description

--- backend/src/app/yasg.py
+++ backend/src/app/yasg.py
@@ -8,7 +8,12 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Areus",
         default_version="v1",
-        description="Test Description",
+        description='''
+        The objective of this project is to develop a simplified web application for managing databases, 
+        inspired by phpMyAdmin. This project, built using the Django framework, emphasizes a clean design, modern 
+        aesthetics, and enhanced performance. The development process will be divided into several steps, gradually 
+        building functionality and refining the user interface.
+        ''',
         license=openapi.License(name="BSD License")
     ),
     public=True,
